6d64b20e3e19596d18f550b346b33429e40d8625
Chris Conley <chrisc93@vt.edu>
2014-05-15 14:27:56 -0400

    So I decided to add a check for the force_fast_charge file...
    Too many people gave me bad reviews on the Play store,
    so this should let them know if they can actually use the app.
    
    Signed-off-by: Chris Conley <chrisc93@vt.edu>

--- src/chris/conley/togglefastcharge/FastCharge.java
+++ src/chris/conley/togglefastcharge/FastCharge.java
@@ -6,15 +6,20 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class FastCharge extends Activity {
 
+	RelativeLayout rl;
 	ToggleButton fcToggle;
+	TextView fcText;
 	File ffc;
 
 	@Override
@@ -22,90 +27,103 @@ public class FastCharge extends Activity {
 		super.onCreate(bundle);
 		setContentView(R.layout.activity_layout);
 
+		rl = (RelativeLayout) findViewById(R.id.rlayout);
 		fcToggle = (ToggleButton) findViewById(R.id.fcToggle);
+		fcText = (TextView) findViewById(R.id.fchargeText);
 		final TextView tv = (TextView) findViewById(R.id.tv1);
 
 		ffc = new File("/sys/kernel/fast_charge/force_fast_charge");
+		if (!ffc.exists()) {
+			fcText.setVisibility(View.GONE);
+			tv.setVisibility(View.GONE);
+			fcToggle.setVisibility(View.GONE);
+			TextView noFC = new TextView(this);
+			noFC.setText("You don't have support for fast charge :(");
+			noFC.setTextSize(25);
+			noFC.setGravity(Gravity.CENTER);
+			rl.setGravity(Gravity.CENTER);
+			rl.addView(noFC);
+		} else {
+			StringBuilder text = new StringBuilder();
 
-		StringBuilder text = new StringBuilder();
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(ffc));
+				String line;
 
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(ffc));
-			String line;
-
-			while ((line = br.readLine()) != null) {
-				text.append(line);
-				text.append('\n');
-			}
-			br.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		tv.setText(text);
-		update(tv.getText().toString());
-
-		fcToggle.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				try {
-					FileOutputStream writer = new FileOutputStream(ffc);
-					if (tv.getText().toString().contains("1"))
-						try {
-							writer.write("0".getBytes());
-							writer.close();
-							StringBuilder text = new StringBuilder();
-
-							try {
-								BufferedReader br = new BufferedReader(
-										new FileReader(ffc));
-								String line;
-
-								while ((line = br.readLine()) != null) {
-									text.append(line);
-									text.append('\n');
-								}
-								br.close();
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-
-							tv.setText(text);
-							update(tv.getText().toString());
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					if (tv.getText().toString().contains("0"))
-						try {
-							writer.write("1".getBytes());
-							writer.close();
-							StringBuilder text = new StringBuilder();
-
-							try {
-								BufferedReader br = new BufferedReader(
-										new FileReader(ffc));
-								String line;
-
-								while ((line = br.readLine()) != null) {
-									text.append(line);
-									text.append('\n');
-								}
-								br.close();
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-
-							tv.setText(text);
-							update(tv.getText().toString());
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
+				while ((line = br.readLine()) != null) {
+					text.append(line);
+					text.append('\n');
 				}
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		});
+
+			tv.setText(text);
+			update(tv.getText().toString());
+
+			fcToggle.setOnClickListener(new View.OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					try {
+						FileOutputStream writer = new FileOutputStream(ffc);
+						if (tv.getText().toString().contains("1"))
+							try {
+								writer.write("0".getBytes());
+								writer.close();
+								StringBuilder text = new StringBuilder();
+
+								try {
+									BufferedReader br = new BufferedReader(
+											new FileReader(ffc));
+									String line;
+
+									while ((line = br.readLine()) != null) {
+										text.append(line);
+										text.append('\n');
+									}
+									br.close();
+								} catch (IOException e) {
+									e.printStackTrace();
+								}
+
+								tv.setText(text);
+								update(tv.getText().toString());
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						if (tv.getText().toString().contains("0"))
+							try {
+								writer.write("1".getBytes());
+								writer.close();
+								StringBuilder text = new StringBuilder();
+
+								try {
+									BufferedReader br = new BufferedReader(
+											new FileReader(ffc));
+									String line;
+
+									while ((line = br.readLine()) != null) {
+										text.append(line);
+										text.append('\n');
+									}
+									br.close();
+								} catch (IOException e) {
+									e.printStackTrace();
+								}
+
+								tv.setText(text);
+								update(tv.getText().toString());
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
 	}
 
 	public void update(String str) {
