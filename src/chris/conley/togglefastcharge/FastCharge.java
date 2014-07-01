d986ad57d902912cb46197d8c191274610b432bc
Chris Conley <chrisc93@vt.edu>
2014-06-30 22:19:06 -0700

    Little code cleanup

--- src/chris/conley/togglefastcharge/FastCharge.java
+++ src/chris/conley/togglefastcharge/FastCharge.java
@@ -15,8 +15,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class FastCharge extends Activity
-{
+public class FastCharge extends Activity {
 	RelativeLayout rl;
 	ToggleButton fcToggle;
 	TextView fcText;
@@ -24,8 +23,7 @@ public class FastCharge extends Activity
 	File ffc;
 
 	@Override
-	public void onCreate(Bundle bundle)
-	{
+	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		setContentView(R.layout.activity_layout);
 
@@ -35,8 +33,7 @@ public class FastCharge extends Activity
 		tv = (TextView) findViewById(R.id.tv1);
 		ffc = new File("/sys/kernel/fast_charge/force_fast_charge");
 
-		if (!ffc.exists())
-		{
+		if (!ffc.exists()) {
 			fcText.setVisibility(View.GONE);
 			tv.setVisibility(View.GONE);
 			fcToggle.setVisibility(View.GONE);
@@ -46,106 +43,81 @@ public class FastCharge extends Activity
 			noFC.setGravity(Gravity.CENTER);
 			rl.setGravity(Gravity.CENTER);
 			rl.addView(noFC);
-		}
-		else
-		{
+		} else {
 			StringBuilder text = new StringBuilder();
 
-			try
-			{
+			try {
 				BufferedReader br = new BufferedReader(new FileReader(ffc));
 				String line;
 
-				while ((line = br.readLine()) != null)
-				{
+				while ((line = br.readLine()) != null) {
 					text.append(line);
 					text.append('\n');
 				}
 				br.close();
-			}
-			catch (IOException e)
-			{
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 
 			tv.setText(text);
 			update(tv.getText().toString());
 
-			fcToggle.setOnClickListener(new View.OnClickListener()
-			{
+			fcToggle.setOnClickListener(new View.OnClickListener() {
 				@Override
-				public void onClick(View v)
-				{
-					try
-					{
+				public void onClick(View v) {
+					try {
 						FileOutputStream writer = new FileOutputStream(ffc);
 						if (tv.getText().toString().contains("1"))
-							try
-							{
+							try {
 								writer.write("0".getBytes());
 								writer.close();
 								StringBuilder text = new StringBuilder();
 
-								try
-								{
+								try {
 									BufferedReader br = new BufferedReader(
 											new FileReader(ffc));
 									String line;
 
-									while ((line = br.readLine()) != null)
-									{
+									while ((line = br.readLine()) != null) {
 										text.append(line);
 										text.append('\n');
 									}
 									br.close();
-								}
-								catch (IOException e)
-								{
+								} catch (IOException e) {
 									e.printStackTrace();
 								}
 
 								tv.setText(text);
 								update(tv.getText().toString());
-							}
-							catch (IOException e)
-							{
+							} catch (IOException e) {
 								e.printStackTrace();
 							}
 						if (tv.getText().toString().contains("0"))
-							try
-							{
+							try {
 								writer.write("1".getBytes());
 								writer.close();
 								StringBuilder text = new StringBuilder();
 
-								try
-								{
+								try {
 									BufferedReader br = new BufferedReader(
 											new FileReader(ffc));
 									String line;
 
-									while ((line = br.readLine()) != null)
-									{
+									while ((line = br.readLine()) != null) {
 										text.append(line);
 										text.append('\n');
 									}
 									br.close();
-								}
-								catch (IOException e)
-								{
+								} catch (IOException e) {
 									e.printStackTrace();
 								}
 
 								tv.setText(text);
 								update(tv.getText().toString());
-							}
-							catch (IOException e)
-							{
+							} catch (IOException e) {
 								e.printStackTrace();
 							}
-					}
-					catch (FileNotFoundException e)
-					{
+					} catch (FileNotFoundException e) {
 						e.printStackTrace();
 					}
 				}
@@ -153,14 +125,11 @@ public class FastCharge extends Activity
 		}
 	}
 
-	public void update(String str)
-	{
-		if (str.contains("1"))
-		{
+	public void update(String str) {
+		if (str.contains("1")) {
 			fcToggle.setChecked(true);
 		}
-		if (str.contains("0"))
-		{
+		if (str.contains("0")) {
 			fcToggle.setChecked(false);
 		}
 	}
